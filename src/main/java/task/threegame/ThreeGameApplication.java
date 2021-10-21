4824ba462fdd8205e69a920b5d37ac02fba8721e
Khaled Ahmed <khaledmamdouh273@gmail.com>
2021-10-21 22:14:42 +0200

    create project

---
+++ src/main/java/task/threegame/ThreeGameApplication.java
@@ -0,0 +1,13 @@
+package task.threegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreeGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreeGameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/task/threegame/ThreeGameApplicationTests.java
@@ -0,0 +1,13 @@
+package task.threegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreeGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
